cac8d64159470855a58e481ef793798b91110afe
Denis Rozimovschii <rozimovschii.denis@yahoo.com>
2017-02-09 23:43:05 +0200

    Added gh-pages lightsel

--- README.md
+++
@@ -1,5 +0,0 @@
-# Lightsel
-### The simpliest carousel for you and your IE8. 
-
-### Please visit [lightsel page](https://beaverden.github.io/lightsel) for documentation, examples
-and installation
---
+++ js/app.js
@@ -0,0 +1,83 @@
+function turnON(id) {
+    var object = document.getElementById(id);
+    object.style.backgroundColor = "rgba(204,0,0, 0.8)";
+    object.style.color = "#fff";
+}
+
+function turnOFF(id) {
+    var object = document.getElementById(id);
+    object.style.backgroundColor = "#fff";
+    object.style.color = "#000";
+}
+
+var activeMovedTimeout = false;
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("lightsel_objectClicked", function(e) {
+
+        turnON("object_clicked");
+        setTimeout(function(){
+            turnOFF("object_clicked");
+        }, 400);
+    });
+
+    document.addEventListener("lightsel_dragStart", function(e) {
+        turnON("drag_start");
+        setTimeout(function(){
+            turnOFF("drag_start");
+        }, 400);
+        turnOFF("drag_end");
+    });
+
+    document.addEventListener("lightsel_dragEnd", function(e) {
+        turnON("drag_end");
+        setTimeout(function() {
+            turnOFF("drag_end");
+        }, 400);
+
+        turnOFF("drag_start");
+        turnOFF("dragged");
+    });
+
+    document.addEventListener("lightsel_dragged", function(e) {
+        turnON("dragged");
+        turnOFF("drag_end");
+        turnON("moved");
+        if (!activeMovedTimeout) {
+            activeMovedTimeout = true;
+            setTimeout(function() {
+                turnOFF("moved");
+                activeMovedTimeout = false;
+            }, 500);
+        }
+
+    });
+
+    document.addEventListener("lightsel_moved", function(e) {
+        turnON("moved");
+        if (!activeMovedTimeout) {
+            activeMovedTimeout = true;
+            setTimeout(function() {
+                turnOFF("moved");
+                activeMovedTimeout = false;
+            }, 500);
+        }
+    });
+
+
+});
+
+
+
+function start() {
+    this.num = 0;
+    this.output = function() {
+        console.log(this.num);
+    }
+    this.change = function() {
+        this.num++;
+    }
+}
+
+
+
